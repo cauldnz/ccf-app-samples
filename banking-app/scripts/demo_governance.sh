83fdcd92f8e7da06c777e9c1088a5a17102ce18e
Chris Auld <chris.auld@auld.nz>
2023-05-23 15:11:10 +0800

    Update banking-app governance to COSE SIGN 1
    Updates governance operations to use COSE SIGN1

--- banking-app/scripts/demo_governance.sh
+++ banking-app/scripts/demo_governance.sh
@@ -38,61 +38,46 @@ cp ../../vote/* ./
 
 # Add users
 
+### user0 ###
 # Proposal for user0
-proposal0_out=$(${ccf_prefix}/scurl.sh ${server}/gov/proposals \
-  --cacert service_cert.pem \
-  --signing-key member0_privk.pem \
-  --signing-cert member0_cert.pem \
-  --data-binary @set_user0.json \
-  -H "content-type: application/json")
+proposal0_out=$(ccf_cose_sign1 --ccf-gov-msg-type proposal --ccf-gov-msg-created_at "$(date -Is)" --signing-key member0_privk.pem --signing-cert member0_cert.pem --content set_user0.json | \
+  curl ${server}/gov/proposals --cacert service_cert.pem --data-binary @- -H "content-type: application/cose")
 proposal0_id=$( jq -r  '.proposal_id' <<< "${proposal0_out}" )
 echo "$proposal0_id"
 
 # Vote by member 1
-$ccf_prefix/scurl.sh ${server}/gov/proposals/"$proposal0_id"/ballots \
-  --cacert service_cert.pem \
-  --signing-key member1_privk.pem \
-  --signing-cert member1_cert.pem \
-  --data-binary @vote_accept.json \
-  -H "content-type: application/json" | jq
+ccf_cose_sign1 --ccf-gov-msg-type ballot --ccf-gov-msg-proposal_id "$proposal0_id" --ccf-gov-msg-created_at "$(date -Is)" --signing-key member1_privk.pem --signing-cert member1_cert.pem --content vote_accept.json | \
+  curl ${server}/gov/proposals/"$proposal0_id"/ballots --cacert service_cert.pem --data-binary @- -H "content-type: application/cose" | jq
 
 # Vote by member 2
-$ccf_prefix/scurl.sh ${server}/gov/proposals/"$proposal0_id"/ballots \
-  --cacert service_cert.pem \
-  --signing-key member2_privk.pem \
-  --signing-cert member2_cert.pem \
-  --data-binary @vote_accept.json \
-  -H "content-type: application/json" | jq
+ccf_cose_sign1 --ccf-gov-msg-type ballot --ccf-gov-msg-proposal_id "$proposal0_id" --ccf-gov-msg-created_at "$(date -Is)" --signing-key member2_privk.pem --signing-cert member2_cert.pem --content vote_accept.json | \
+  curl ${server}/gov/proposals/"$proposal0_id"/ballots --cacert service_cert.pem --data-binary @- -H "content-type: application/cose" | jq
 
+
+### user1 ###
 # Proposal for user1
-proposal1_out=$(${ccf_prefix}/scurl.sh ${server}/gov/proposals --cacert service_cert.pem --signing-key member0_privk.pem --signing-cert member0_cert.pem --data-binary @set_user1.json -H "content-type: application/json")
+proposal1_out=$(ccf_cose_sign1 --ccf-gov-msg-type proposal --ccf-gov-msg-created_at "$(date -Is)" --signing-key member0_privk.pem --signing-cert member0_cert.pem --content set_user1.json | \
+  curl ${server}/gov/proposals --cacert service_cert.pem --data-binary @- -H "content-type: application/cose")
 proposal1_id=$( jq -r  '.proposal_id' <<< "${proposal1_out}" )
-echo "$proposal0_id"
+echo "$proposal1_id"
 
 # Vote by member 1
-$ccf_prefix/scurl.sh ${server}/gov/proposals/"$proposal1_id"/ballots \
-  --cacert service_cert.pem \
-  --signing-key member1_privk.pem \
-  --signing-cert member1_cert.pem \
-  --data-binary @vote_accept.json \
-  -H "content-type: application/json" | jq
+ccf_cose_sign1 --ccf-gov-msg-type ballot --ccf-gov-msg-proposal_id "$proposal1_id" --ccf-gov-msg-created_at "$(date -Is)" --signing-key member1_privk.pem --signing-cert member1_cert.pem --content vote_accept.json | \
+  curl ${server}/gov/proposals/"$proposal1_id"/ballots --cacert service_cert.pem --data-binary @- -H "content-type: application/cose" | jq
 
 # Vote by member 2
-$ccf_prefix/scurl.sh ${server}/gov/proposals/"$proposal1_id"/ballots \
-  --cacert service_cert.pem \
-  --signing-key member2_privk.pem \
-  --signing-cert member2_cert.pem \
-  --data-binary @vote_accept.json \
-  -H "content-type: application/json" | jq
+ccf_cose_sign1 --ccf-gov-msg-type ballot --ccf-gov-msg-proposal_id "$proposal1_id" --ccf-gov-msg-created_at "$(date -Is)" --signing-key member2_privk.pem --signing-cert member2_cert.pem --content vote_accept.json | \
+  curl ${server}/gov/proposals/"$proposal1_id"/ballots --cacert service_cert.pem --data-binary @- -H "content-type: application/cose" | jq
+
 
 # "Display network ccf version"
-curl "${server}/node/version" --cacert service_cert.pem
+curl "${server}/node/version" --cacert service_cert.pem | jq
 
 # "Display network details"
-curl "${server}/node/network" --cacert service_cert.pem
+curl "${server}/node/network" --cacert service_cert.pem | jq
 
 # "Display network nodes details"
-curl "${server}/node/network/nodes" --cacert service_cert.pem
+curl "${server}/node/network/nodes" --cacert service_cert.pem | jq
 
 # "Display network members details"
-curl "${server}/gov/members" --cacert service_cert.pem
+curl "${server}/gov/members" --cacert service_cert.pem | jq
